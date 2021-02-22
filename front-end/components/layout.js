9e366dbcc820ecf66b5491262a4120ea2a9abc78
ndemo-richard <ndemorich@gmail.com>
2021-02-22 20:48:19 +0300

    remove sumo

--- front-end/components/layout.js
+++ front-end/components/layout.js
@@ -9,14 +9,17 @@ const Layout = props =>{
         <div className={styles.Layout}>
             
             <Header/>
+         
 
             <div >{props.children}</div>
 
            {/* <Footer footer={props.children}/>*/}
-           <script async>(function(s,u,m,o,j,v){j=u.createElement(m);v=u.getElementsByTagName(m)[0];j.async=1;j.src=o;j.dataset.sumoSiteId='11c7d72fc2c125da1d8799142bc59fd791abcaf71b6aa9840a0698f5c5eb5c2f';v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/');</script>
+          
         </div>
+        
 
     );
+    
 };
 
 export default Layout;
